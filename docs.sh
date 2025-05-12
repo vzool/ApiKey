27bad07f3bcb6724724f4d2fcb6f9002ea056f1e
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2025-05-12 16:38:37 +0300

    حذف تطبيق مولد التقارير وتنزيله في حالة أنه غير موجود

--- docs.sh
+++ docs.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 rm -rf .phpdoc ./docs
-./phpDocumentor.phar -v --validate --sourcecode --title="ApiKey" -f ApiKey.php -t ./docs
+
+if [ ! -f phpDocumentor.phar ]; then
+  echo "phpDocumentor.phar not found. Downloading..."
+  curl -Lo phpDocumentor.phar https://phpdoc.org/phpDocumentor.phar
+  if [ $? -eq 0 ]; then
+    echo "phpDocumentor.phar downloaded successfully."
+  else
+    echo "Error downloading phpDocumentor.phar."
+    exit 1
+  fi
+fi
+
+echo "phpDocumentor.phar exists."
+
+php phpDocumentor.phar -v --validate --sourcecode --title="ApiKey" -f ApiKey.php -t ./docs
